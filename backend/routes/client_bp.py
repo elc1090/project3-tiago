d4d0afd9bf24bef6630c9a3e73192d4f1095a160
Tiago Ribeiro Chagas <49379007+trchagas@users.noreply.github.com>
2023-05-30 07:59:30 -0300

    Delete backend directory

--- backend/routes/client_bp.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-from controllers.ClientController import ClientController
-
-client_bp = Blueprint('client_bp', __name__)
-
-client_bp.route('', methods=['POST'])(ClientController.store)
--- backend/routes/client_client_bp.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-from controllers.Client.ClientController import ClientController
-
-client_client_bp = Blueprint('client_client_bp', __name__)
-
-client_client_bp.route('', methods=['GET'])(ClientController.index)
-client_client_bp.route('/<int:id>',
-                       methods=['PUT'])(ClientController.update)
