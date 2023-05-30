412d6938d92822f7021b18ee71ddc8c681085e22
trchagas <trchagas21@gmail.com>
2023-05-30 08:05:06 -0300

    fix front deploy

--- dist/js/chunk-2d0b6746.9605f5db.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b6746"],{"1cdb":function(n,u,c){"use strict";c.r(u);var l,e,o=c("2877"),p={},s=Object(o["a"])(p,l,e,!1,null,null,null);u["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d0b6746.9605f5db.js.map
