8d2c1d59500783359bbfc127130288f496f90e85
trchagas <trchagas21@gmail.com>
2023-05-30 02:47:23 -0300

    update deploy file

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -16,10 +16,10 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# if you are deploying to https://trchagas.github.io
+# git push -f git@github.com:trchagas/trchagas.github.io.git main
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+# if you are deploying to https://trchagas.github.io/<REPO>
+# git push -f git@github.com:trchagas/<REPO>.git main:gh-pages
 
 cd -
